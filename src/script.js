33c4438796f6d011b0db7c730f686e8cd6f04c68
ussef <ussefbouchraa@gmail.com>
2025-08-14 11:03:31 +0100

    little optimise

--- src/script.js
+++ src/script.js
@@ -2,20 +2,6 @@ import { Player } from "../Player.js";
 
 const player = new Player();
 
-document.addEventListener("keydown", (event) => {
-    if (event.key === "ArrowLeft") {
-        player.direction = "left";
-    } else if (event.key === "ArrowRight") {
-        player.direction = "right";
-    }
-});
-
-document.addEventListener("keyup", () => {
-   console.log("now");
-   
-    player.direction = null;
-});
-
 
 
 function gameLoop() {
