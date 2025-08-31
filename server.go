96f66bc99fd66e42f0d73f04c2fa643c90afb09b
ussef <ussefbouchraa@gmail.com>
2025-08-31 12:50:58 +0100

    initial structure for Score-Handling project

---
+++ server.go
@@ -0,0 +1,24 @@
+package game
+
+
+import (
+  "encoding/json"
+  "net/http"
+  "os"
+  "sort"
+
+
+)
+type Score struct {
+    Name  string `json:"name"`
+    Score int    `json:"score"`
+    Time  string `json:"time"`
+}
+
+func main(){
+
+
+
+
+
+}
